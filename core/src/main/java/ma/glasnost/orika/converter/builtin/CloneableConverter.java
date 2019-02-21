59c9a03f3736e314b30abaeaed7b22924396fe5c
Sidi Mohamed EL AATIFI <username@users.noreply.github.com>
2019-02-21 12:29:46 +0100

    Update license headers

--- core/src/main/java/ma/glasnost/orika/converter/builtin/CloneableConverter.java
+++ core/src/main/java/ma/glasnost/orika/converter/builtin/CloneableConverter.java
@@ -1,19 +1,19 @@
 /*
  * Orika - simpler, better and faster Java bean mapping
  *
- * Copyright (C) 2011-2013 Orika authors
+ *  Copyright (C) 2011-2019 Orika authors
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package ma.glasnost.orika.converter.builtin;
 
@@ -39,7 +39,6 @@ import ma.glasnost.orika.metadata.TypeFactory;
  * This allows you to declare your own set of types which should be cloned
  * instead of mapped.
  * 
- * @author matt.deboer@gmail.com
  *
  */
 public class CloneableConverter extends CustomConverter<Object, Object> {
@@ -48,9 +47,7 @@ public class CloneableConverter extends CustomConverter<Object, Object> {
     private final Map<Class<?>, Method> cachedMethods;
     private final Method cloneMethod;
     private final String description;
-    static {
-        //Object.class.getModule().addOpens("java.base", CloneableConverter.class.getModule());
-    }
+
     /**
      * Constructs a new ClonableConverter configured to handle the provided list
      * of types by cloning.
@@ -67,7 +64,7 @@ public class CloneableConverter extends CustomConverter<Object, Object> {
             methodCache = null;
         } catch (SecurityException e) {
             clone = null;
-            methodCache = new WeakHashMap<Class<?>, Method>();
+            methodCache = new WeakHashMap<>();
         } catch (NoSuchMethodException e) {
             throw new IllegalStateException(e);
         }
